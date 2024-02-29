1858279679f516e021536071eddea60f9f0c6963
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2024-02-29 22:33:10 +0300

    003 realized function BUILD task

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main(   )
 {
-cout<<"hi"<<endl;
+cout<<"hi. input X1"<<endl;
 int num=100;
 int num2;
 cin>>num;
 //num = num +1 ;
 cout << num<< endl;
-num2 = 3;
+cout<<"hi. input X2"<<endl;
 cin>>num2;
+num2 = 3*2;
 cout << num2 << endl;
 
 return 0;
