2d825c721b839201000c7ad69c20ef3bfd928371
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2024-03-01 05:36:12 +0300

    004 added PR4 plus adjusted debugger

---
+++ Project4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int fav_num;
+cout<<"Ent your fav num btw 1 and 100"<<endl;
+cin>>fav_num;
+cout<<"amazing";
+cout<<"bla bla"<<fav_num<<"bla"<<endl;
+return 0;
+}
