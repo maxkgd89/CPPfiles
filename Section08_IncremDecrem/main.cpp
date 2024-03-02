d7045e60902242676e531a1c5e434b84f596baed
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2024-03-03 00:51:43 +0300

    011 Section 8 reworked

--- Section08_IncremDecrem/main.cpp
+++ Section08_IncremDecrem/main.cpp
@@ -34,46 +34,45 @@ int main() {
     cout << "Counter : " << counter << endl;
 
 // Example 2 - preincrement
-//    counter = 10;
-//    result = 0;
-//    
-//    cout << "Counter : " << counter << endl;
-//    
-//    result = ++counter; // Note the pre increment
-//    cout << "Counter : " << counter << endl;
-//    cout << "Result : " << result << endl;
+   counter = 10;
+    result = 0;
+    
+    cout << "Counter : " << counter << endl;
+    
+    result = ++counter; // Note the pre increment
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
 // Example 3 - post-increment
-//    counter = 10;
-//    result = 0;
-//    
-//    cout << "Counter : " << counter << endl;
-//    
-//    result = counter++; // Note the post increment
-//    cout << "Counter : " << counter << endl;
-//    cout << "Result : " << result << endl;
+    counter = 10;
+    result = 0;
+    
+    cout << "Counter : " << counter << endl;
+    
+    result = counter++; // Note the post increment
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
 // Example 4
-//    counter = 10;
-//    result = 0;
-//
-//    cout << "Counter : " << counter << endl;
-//
-//    result = ++counter + 10;  // Note the pre increment
-//        
-//    cout << "Counter : " << counter << endl;
-//    cout << "Result : " << result << endl;
+    counter = 10;
+    result = 0;
+
+    cout << "Counter : " << counter << endl;
+
+    result = ++counter + 10;  // Note the pre increment
+        
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
 // Example 5
-//    counter = 10;
-//    result = 0;
-//
-//    cout << "Counter : " << counter << endl;
-//
-//    result = counter++ + 10;  // Note the post increment
-//        
-//    cout << "Counter : " << counter << endl;
-//    cout << "Result : " << result << endl;
+    counter = 10;
+    result = 0;
+
+    cout << "Counter : " << counter << endl;//
+    result = counter++ + 10;  // Note the post increment
+        
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
     cout << endl;
     return 0;
