02c3d70d4fe23712ac72a7e4d3acfd7b93232065
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-05-20 00:24:03 +0300

    misc

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(   )
 {
 cout<<"hi. input X1"<<endl;
-int num=100;
+int num1=100;
 int num2=10;
-cin>>num;
+cin>>num1;
 //num = num +1 ;
-cout << num<< endl;
+cout << num1<< endl;
 cout<<"hi. input X2"<<endl;
 cin>>num2;
-num2 = 3*2;
+num2 = 3*10;
 cout << num2 << endl;
-
+cout << num1+num2<<endl;
 return 0;
 }
